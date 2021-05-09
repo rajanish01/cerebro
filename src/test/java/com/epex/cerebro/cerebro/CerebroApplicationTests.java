e930c11eee4b24a2b413249aa2938b08dea19b95
Rajanish <rajanishatwork@gmail.com>
2021-05-09 21:11:52 +0530

    initial commit
    Signed-off-by: Rajanish <rajanishatwork@gmail.com>

---
+++ src/test/java/com/epex/cerebro/cerebro/CerebroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epex.cerebro.cerebro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerebroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
