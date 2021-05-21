e6dd399d729eca9c7becab9d32ea0766a2117037
Rajanish <rajanishatwork@gmail.com>
2021-05-22 03:50:05 +0530

    test skeleton for unit test
    Signed-off-by: Rajanish <rajanishatwork@gmail.com>

---
+++ lib/src/test/java/com/epex/cerebro/PawnMoveGeneratorTest.java
@@ -0,0 +1,19 @@
+package com.epex.cerebro;
+
+import com.epex.cosmos.domain.Game;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PawnMoveGeneratorTest {
+
+    private Game game;
+
+    @Before
+
+
+    @Test
+    public void pawnPositionBasicTest(){
+
+    }
+
+}
